37f4144c023dfbf1cfbddf62b31132d214ad1414
magic8baller <lturis@gmail.com>
2018-10-18 07:50:12 -0400

    first stab

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    @@all = []
+
+    def initialize(attributes)
+      attributes.each {|key, value| self.send(("#{key}="), value)}
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.save(name, type, database_connection)
+      database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",
+      name, type)
+    end
+
+    def self.find(id, database_connection)
+      database_connection.execute("SELECT * FROM pokemon WHERE pokemon.id = ?")
+    end
+
 end
